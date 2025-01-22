7214c39d1de9b3f78dfd646e09f0ae826b03b7ec
diratche <dimitriiratchet@yahoo.fr>
2025-01-22 07:22:30 +0100

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -8,9 +8,15 @@ import pandas as pd
 server = sys.argv[1]
 key = sys.argv[2]
 topic = sys.argv[3]
-oblasts = json.load(sys.argv[4])
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+oblasts = ['Donetsk',
+            'Luhansk',
+            'Dnipropetrovsk',
+            'Zaporizhzhya',
+            'Kharkiv',
+            'Kherson',
+            'Sumy',]
 ob_id = len(oblasts) - 1
 while True:
   if ob_id == len(oblasts) - 1:
