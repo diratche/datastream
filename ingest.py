16ea133613c1cd46d711e357e6f176d737b7a92d
diratche <dimitriiratchet@yahoo.fr>
2025-01-22 07:21:28 +0100

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -8,7 +8,7 @@ import pandas as pd
 server = sys.argv[1]
 key = sys.argv[2]
 topic = sys.argv[3]
-oblasts = sys.argv[4]
+oblasts = json.load(sys.argv[4])
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 ob_id = len(oblasts) - 1
@@ -22,7 +22,7 @@ while True:
   while dt < pd.to_datetime('today'):
     dt = dt + pd.Timedelta(days=1)
     dt_string = dt.strftime('%Y-%m-%d')
-    address = server + '?key=' + key + '&q=Donetsk' + '&dt=' + dt_string + '&end_dt=' + dt_string
+    address = server + '?key=' + key + '&q=' + oblast + '&dt=' + dt_string + '&end_dt=' + dt_string
     data = requests.get(address)
     if data.status_code == 200:
       data = data.json()
