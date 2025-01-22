96882a3d044c5e509d60afd6fcced1e3815a7549
diratche <dimitriiratchet@yahoo.fr>
2025-01-22 07:14:38 +0100

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -8,14 +8,23 @@ import pandas as pd
 server = sys.argv[1]
 key = sys.argv[2]
 topic = sys.argv[3]
+oblasts = sys.argv[4]
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: json.dumps(x).encode('utf-8'))
-dt = pd.to_datetime('today') - pd.Timedelta(days=365)
-while dt < pd.to_datetime('today'):
-  dt = dt + pd.Timedelta(days=1)
-  dt_string = dt.strftime('%Y-%m-%d')
-  address = server + '?key=' + key + '&q=Donetsk' + '&dt=' + dt_string + '&end_dt=' + dt_string
-  data = requests.get(address)
-  if data.status_code == 200:
-    data = data.json()
-    print(data['forecast'])
+ob_id = len(oblasts) - 1
+while True:
+  if ob_id == len(oblasts) - 1:
+    ob_id = 0
+  else:
+    ob_id += 1
+  oblast = oblasts[ob_id]
+  dt = pd.to_datetime('today') - pd.Timedelta(days=365)
+  while dt < pd.to_datetime('today'):
+    dt = dt + pd.Timedelta(days=1)
+    dt_string = dt.strftime('%Y-%m-%d')
+    address = server + '?key=' + key + '&q=Donetsk' + '&dt=' + dt_string + '&end_dt=' + dt_string
+    data = requests.get(address)
+    if data.status_code == 200:
+      data = data.json()
+      data['forecast']['oblast'] = oblast
+      print(data['forecast'])
